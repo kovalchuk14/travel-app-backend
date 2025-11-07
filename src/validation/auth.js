159f7a79e5722dfa9d68c6d5fd812238c04389f9
ComeONDuDE18 <vibilous6@gmail.com>
2025-11-07 13:05:59 +0100

    register

---
+++ src/validation/auth.js
@@ -0,0 +1,7 @@
+import Joi from 'joi';
+
+export const registerUserSchema = Joi.object({
+  name: Joi.string().trim().min(3).max(32).required(),
+  email: Joi.string().email().trim().max(64).required(),
+  password: Joi.string().min(8).max(128).required(),
+});
