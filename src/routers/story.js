6d1895b85fdf590f371b21277050718215875e42
Lerros <108396158+LerrosGr@users.noreply.github.com>
2025-11-09 14:13:04 +0200

    moving file /validation/stories.js

--- src/routers/story.js
+++ src/routers/story.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 import { validateBody } from '../middlewares/validateBody.js';
-import { createStorySchema } from '../../validation/stories.js';
+import { createStorySchema } from '../validation/stories.js';
 import { createStoryController } from '../controllers/story.js';
 
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
