b140c28823aab7f451b79a011d096ba2c70f0876
ComeONDuDE18 <vibilous6@gmail.com>
2025-11-10 15:57:53 +0100

    Comment out getCurrentUser function
    Comment out the getCurrentUser function in user.js

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,6 @@
 import { getUserById } from '../services/user.js';
 
-export const getCurrentUser = async (req, res) => {
+/*export const getCurrentUser = async (req, res) => {
   const user = req.user;
   res.status(200).json({
     status: 200,
@@ -8,6 +8,7 @@ export const getCurrentUser = async (req, res) => {
     data: user,
   });
 };
+*/
 
 export const getUserByIdController = async (req, res) => {
     const { userId } = req.params;
