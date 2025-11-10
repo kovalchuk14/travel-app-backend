358f734c7c8c40020b4647164c5da34ce5e4d689
kovalchuk14 <kovalcukanton682@gmail.com>
2025-11-10 16:55:31 +0100

    rename IsValidId.js to isValidId.js

---
+++ src/middlewares/isValidId.js
@@ -0,0 +1,10 @@
+import { isValidObjectId } from "mongoose";
+import createHttpError from "http-errors";
+
+export const isValidId = (req, res, next) => {
+    const { objectId } = req.params;
+    if (!isValidObjectId(objectId)) {
+        throw createHttpError(400, 'Bad Request')
+    };
+    next();
+}
