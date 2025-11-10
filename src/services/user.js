63a068100786d02fc36f7b4b5d4b29341e2170ba
ComeONDuDE18 <vibilous6@gmail.com>
2025-11-10 15:53:27 +0100

    Delete user service functions
    Removed user-related functions from user.js

--- src/services/user.js
+++ src/services/user.js
@@ -1,11 +1,1 @@
-import { UsersCollection } from "../db/models/user.js";
 
-export const getCurrentUser = async () => {
-const users = await UsersCollection.find({});
-return users;
-}
-
-export const getUserById = async (userId) => {
-  const user = await UsersCollection.findById(userId);
-  return user;
-}
