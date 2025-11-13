5848e4a2c65c27298dbf8bc6a091ebe37c68ba2d
kovalchuk14 <kovalcukanton682@gmail.com>
2025-11-13 13:55:33 +0100

    /

--- src/routers/index.js
+++ src/routers/index.js
@@ -8,6 +8,7 @@ const router = Router();
 
 router.use('/users', userRouter);
 router.use('/auth', authRouter);
-router.use('/stories', storyRouter);router.use('/users', usersRouter);
+router.use('/stories', storyRouter);
+router.use('/users', usersRouter);
 
 export default router;
